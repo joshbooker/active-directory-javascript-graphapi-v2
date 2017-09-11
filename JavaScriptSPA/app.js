e340521ff47cff30c20bd96b8f2e79589a1204b0
JoshuaABooker <JoshuaABooker@DESKTOP-R5ANG0N>
2017-09-11 12:15:27 -0400

    inviteUserAddGroupMember

--- JavaScriptSPA/app.js
+++ JavaScriptSPA/app.js
@@ -1,8 +1,12 @@
 ﻿// Graph API endpoint to show user profile
 var graphApiEndpoint = "https://graph.microsoft.com/v1.0/me";
 
+//admin consent endpoint example
+//https://login.microsoftonline.com/common/adminconsent?client_id=39683401-a60c-4bc6-b744-6dbaedef498d&state=12345&redirect_uri=http://localhost:30662/
+
 // Graph API scope used to obtain the access token to read user profile
 var graphAPIScopes = ["https://graph.microsoft.com/user.read"];
+
 
 // Initialize application
 var userAgentApplication = new Msal.UserAgentApplication(msalconfig.clientID, null, loginCallback, {
@@ -15,11 +19,12 @@ if (userAgentApplication.redirectUri) {
 }
 
 window.onload = function () {
-    // If page is refreshed, continue to display user info
+     //If page is refreshed, continue to display user info
     if (!userAgentApplication.isCallback(window.location.hash) && window.parent === window && !window.opener) {
         var user = userAgentApplication.getUser();
         if (user) {
-            callGraphApi();
+            //callGraphApi();
+            callGraphApi2();
         }
     }
 }
@@ -31,10 +36,9 @@ window.onload = function () {
 function callGraphApi() {
     var user = userAgentApplication.getUser();
     if (!user) {
-        // If user is not signed in, then prompt user to sign in via loginRedirect.
+        //If user is not signed in, then prompt user to sign in via loginRedirect.
         // This will redirect user to the Azure Active Directory v2 Endpoint
         userAgentApplication.loginRedirect(graphAPIScopes);
-
         // The call to loginRedirect above frontloads the consent to query Graph API during the sign-in.
         // If you want to use dynamic consent, just remove the graphAPIScopes from loginRedirect call:
         // As such, user will be prompted to give consent as soon as the token for a resource that 
@@ -61,6 +65,69 @@ function callGraphApi() {
             .then(function (token) {
                 //After the access token is acquired, call the Web API, sending the acquired token
                 callWebApiWithToken(graphApiEndpoint, token, graphCallResponseElement, document.getElementById("accessToken"));
+            }, function (error) {
+                // If the acquireTokenSilent() method fails, then acquire the token interactively via acquireTokenRedirect().
+                // In this case, the browser will redirect user back to the Azure Active Directory v2 Endpoint so the user 
+                // can re-type the current username and password and/ or give consent to new permissions your application is requesting.
+                // After authentication/ authorization completes, this page will be reloaded again and callGraphApi() will be called.
+                // Then, acquireTokenSilent will then acquire the token silently, the Graph API call results will be made and results will be displayed in the page.
+                if (error) {
+                    userAgentApplication.acquireTokenRedirect(graphAPIScopes);
+                    //userAgentApplication.acquireTokenRedirect();
+                }
+            });
+
+    }
+}
+function callGraphApi2() {
+    var user = userAgentApplication.getUser();
+    if (!user) {
+        // If user is not signed in, then prompt user to sign in via loginRedirect.
+        // This will redirect user to the Azure Active Directory v2 Endpoint
+        userAgentApplication.loginRedirect(graphAPIScopes);
+        //userAgentApplication.loginRedirect();
+        // The call to loginRedirect above frontloads the consent to query Graph API during the sign-in.
+        // If you want to use dynamic consent, just remove the graphAPIScopes from loginRedirect call:
+        // As such, user will be prompted to give consent as soon as the token for a resource that 
+        // he/she hasn't consented before is requested. In the case of this application - 
+        // the first time the Graph API call to obtain user's profile is executed.
+    } else {
+
+        // If user is already signed in, display the user info
+        var userInfoElement = document.getElementById("userInfo");
+        userInfoElement.parentElement.classList.remove("hidden");
+        userInfoElement.innerHTML = JSON.stringify(user, null, 4);
+
+        // Show Sign-Out button
+        document.getElementById("signOutButton").classList.remove("hidden");
+
+        // Now Call Graph API to show the user profile information:
+        var graphCallResponseElement = document.getElementById("graphResponse");
+        graphCallResponseElement.parentElement.classList.remove("hidden");
+        graphCallResponseElement.innerText = "Calling Graph ...";
+
+        // In order to call the Graph API, an access token needs to be acquired.
+        // Try to acquire the token used to Query Graph API silently first
+        userAgentApplication.acquireTokenSilent(graphAPIScopes)
+            .then(function (token) {
+                //After the access token is acquired, call the Web API, sending the acquired token
+                //callWebApiWithToken(graphApiEndpoint, token, graphCallResponseElement, document.getElementById("accessToken"));
+                var body =    {
+                                "invitedUserDisplayName": "Josh2",
+                                "invitedUserEmailAddress": "jbooker@midcoast.com",
+                                "inviteRedirectUrl": "http://localhost:30662/",
+                                "sendInvitationMessage": true,
+                                "invitedUserMessageInfo": {
+                                    "ccRecipients": [{
+                                        "emailAddress": {
+                                            "address": "josh@joshbooker.com",
+                                            "name": "AdminJosh"
+                                        }
+                                    }],
+                                    "customizedMessageBody": "Hi Josh2, you can find more information here: http://localhost:30662/"
+                                }
+                                };
+                var result = inviteUserAddGroupMember(graphInvitationEndpoint, "POST", body, token, graphCallResponseElement, document.getElementById("accessToken"));
 
             }, function (error) {
                 // If the acquireTokenSilent() method fails, then acquire the token interactively via acquireTokenRedirect().
@@ -70,6 +137,7 @@ function callGraphApi() {
                 // Then, acquireTokenSilent will then acquire the token silently, the Graph API call results will be made and results will be displayed in the page.
                 if (error) {
                     userAgentApplication.acquireTokenRedirect(graphAPIScopes);
+                    //userAgentApplication.acquireTokenRedirect();
                 }
             });
 
@@ -88,7 +156,7 @@ function showError(endpoint, error, errorDesc) {
         formattedError = error;
     }
     document.getElementById("errorMessage").innerHTML = "An error has occurred:<br/>Endpoint: " + endpoint + "<br/>Error: " + formattedError + "<br/>" + errorDesc;
-    console.error(error);
+    //console.error(error);
 }
 
 /**
@@ -106,6 +174,86 @@ function loginCallback(errorDesc, token, error, tokenType) {
     }
 }
 
+/*
+ * Call a Web API using an access token.
+ * 
+ * @param {any} endpoint - Web API endpoint
+ * @param {any} token - Access token
+ * @param {object} responseElement - HTML element used to display the results
+ * @param {object} showTokenElement = HTML element used to display the RAW access token
+ * https://github.com/github/fetch
+ */
+function inviteUserAddGroupMember(endpoint, method, body, token, responseElement, showTokenElement) {
+    var headers = new Headers();
+    var bearer = "Bearer " + token;
+    headers.append("Authorization", bearer);
+    headers.append("Content-Type", 'application/json');
+    var options = {
+        method: method,
+        headers: headers,
+        body: JSON.stringify(body)
+    };
+
+    fetch(endpoint, options)
+        .then(function (response) {
+            var contentType = response.headers.get("content-type");
+            if ((response.status === 200 || response.status === 201) && contentType && contentType.indexOf("application/json") !== -1) {
+                response.json()
+                    .then(function (data) {
+                        // Display response in the page
+                        console.log(data);
+                        responseElement.innerHTML = JSON.stringify(data, null, 4);
+                        if (showTokenElement) {
+                            showTokenElement.parentElement.classList.remove("hidden");
+                            showTokenElement.innerHTML = token;
+                        }
+                        // add invited user to External group
+                        endpoint = msalconfig.graphGroupMembersEndpoint.replace("{groupId}",  msalconfig.groupId);
+                        options.body =  JSON.stringify({
+                            "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/" + data.invitedUser.id
+                        });
+                        fetch(endpoint, options)
+                            .then(function (response) {
+                                var contentType = response.headers.get("content-type");
+                                if ((response.status === 204)) {
+                                            responseElement.innerHTML += "User Added to Group";
+                                } else {
+                                    response.json()
+                                        .then(function (data) {
+                                            if (response.status === 400 && data.error.message === "One or more added object references already exist for the following modified properties: 'members'."){
+                                                responseElement.innerHTML += "User Already in Group";
+                                            } else {
+                                            // Display response as error in the page
+                                                showError(endpoint, data);
+                                            }
+                                        })
+                                        .catch(function (error) {
+                                            showError(endpoint, error);
+                                        });
+                                }
+                            })
+                            .catch(function (error) {
+                                showError(endpoint, error);
+                            });
+                    })
+                    .catch(function (error) {
+                        showError(endpoint, error);
+                    });
+            } else {
+                response.json()
+                    .then(function (data) {
+                        // Display response as error in the page
+                        showError(endpoint, data);
+                    })
+                    .catch(function (error) {
+                        showError(endpoint, error);
+                    });
+            }
+        })
+        .catch(function (error) {
+            showError(endpoint, error);
+        });
+}
 /**
  * Call a Web API using an access token.
  * 
--- JavaScriptSPA/msalconfig.js
+++ JavaScriptSPA/msalconfig.js
@@ -1,4 +1,13 @@
 ﻿var msalconfig = {
-    clientID: "Enter_the_Application_Id_here",
-    redirectUri: location.origin
+    graphInvitationEndpoint: "https://graph.microsoft.com/beta/invitations",
+    graphGroupMembersEndpoint: "https://graph.microsoft.com/v1.0/groups/{groupId}/members/$ref",
+    graphAPIScopes: ["https://graph.microsoft.com/.default"],
+    authorityHostUrl: "https://login.microsoftonline.com",
+    common: "common/oauth2/v2.0",
+    tenant: "infoconsulting.onmicrosoft.com",
+    //authorityUrl: msalconfig.authorityHostUrl + '/' + msalconfig.common,
+    clientID: "39683401-a60c-4bc6-b744-6dbaedef498d",
+    secret: "NTjj9dYf2KDCxdZRCVLMXex",
+    redirectUri: location.origin,
+    groupId: "c3346f2f-94d5-401a-afb1-9ad77616d79f"
 };
